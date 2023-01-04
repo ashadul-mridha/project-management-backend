721416095788cf370e946dfe6506f55ecba25426
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2023-01-04 16:30:33 +0600

    task priority list change in db

--- models/taskModel.js
+++ models/taskModel.js
@@ -26,7 +26,7 @@ module.exports = (sequelize , DataTypes) => {
             allowNull: false
         },
         priority: {
-            type: DataTypes.ENUM("first", "second", "thired","four"),
+            type: DataTypes.ENUM("first", "second", "thired"),
             defaultValue: "four",
         },
         remain: {
