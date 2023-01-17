bbb2fc59b0a1f802b8a7020fc4ae9d578aedb2a0
Ashadul Mridha <ashadulmridhaprog@gmail.com>
2023-01-17 12:56:23 +0600

    password forgot email send with html

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,6 @@ const jwt = require("jsonwebtoken");
 const uploadFolder = path.join( __dirname , '/../public/images/uploads/user');
 const nodemailer = require("nodemailer");
 const smtpConfig = require("../config/smtpConfig");
-const resetPasswordMail = require("../mail/resetPassword");
 
 //Import model
 const User = db.user;
@@ -212,26 +211,217 @@ const forgotPassword = async (req, res) => {
 
 				if (forgotDatainsert) {
 
-					res.send({
-							status: true,
-							message: "email sent successfull",
-							data : resetPasswordLink,
-							statusCode: 200
-					})
+					// create transport and verify
+					let transporter = nodemailer.createTransport(smtpConfig);
+        			const verifyTranspoter = await transporter.verify();
+
+					if (verifyTranspoter) {
+
+						const sendmail = async () => {
+
+							let info = await transporter.sendMail({
+								from: '<ashadul@wasa.decode-lab.com>', // sender address
+								to: user.email,  // list of receivers
+								subject: "Reset your Project Management password", // Subject line
+								html: `
+								<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+				
+									<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+										style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+										<tr>
+											<td>
+												<table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+													align="center" cellpadding="0" cellspacing="0">
+													<tr>
+														<td style="height:80px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td style="text-align:center;">
+														<a href="https://decode-lab.com" title="logo" target="_blank">
+															<img width="80" src="https://i.ibb.co/Rh3XBsf/decode-lab.png" title="logo" alt="logo">
+														</a>
+														</td>
+													</tr>
+													<tr>
+														<td style="height:20px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td>
+															<table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+																style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+																<tr>
+																	<td style="height:40px;">&nbsp;</td>
+																</tr>
+																<tr>
+																	<td style="padding:0 35px;">
+																		<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">${user.name} you have
+																			requested to reset your password</h1>
+																		<span
+																			style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+																			We cannot simply send you your old password. A unique link to reset your
+																			password has been generated for you. To reset your password, click the
+																			following link and follow the instructions.
+																		</p>
+																		<a href=${resetPasswordLink}
+																			style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+																			Password</a>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px;">
+																			If you didn’t request a password reset, ignore this email. Your password will stay the same.
+																		</p>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px; text-align: left;">
+																			Productively, </br> The Decode Lab Team
+																		</p>
+																	
+																	</td>
+																
+																</tr>
+																<tr>
+																	<td style="height:40px;">&nbsp;</td>
+																</tr>
+															</table>
+														</td>
+													<tr>
+														<td style="height:20px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td style="text-align:center;">
+															<p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.decode-lab.com</strong></p>
+														</td>
+													</tr>
+													<tr>
+														<td style="height:80px;">&nbsp;</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+								</body>
+								`
+							});
+
+							if (info.response) {
+								res.send({
+										status: true,
+										message: "email sent successfull",
+										data : resetPasswordLink,
+										statusCode: 200
+								})
+							}
+
+						}
+						sendmail();
+					}
 
 				}
+
 			} else {
 
 				const forgotDatainsert = await ForgotPass.create( insertData );
 
 				if (forgotDatainsert) {
 
-					res.send({
-							status: true,
-							message: "email sent successfull",
-							data : resetPasswordLink,
-							statusCode: 200
-					})
+					// create transport and verify
+					let transporter = nodemailer.createTransport(smtpConfig);
+        			const verifyTranspoter = await transporter.verify();
+
+					if (verifyTranspoter) {
+
+						const sendmail = async () => {
+
+							let info = await transporter.sendMail({
+								from: '<ashadul@wasa.decode-lab.com>', // sender address
+								to: user.email,  // list of receivers
+								subject: "Reset your Project Management password", // Subject line
+								html: `
+								<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+				
+									<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+										style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+										<tr>
+											<td>
+												<table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+													align="center" cellpadding="0" cellspacing="0">
+													<tr>
+														<td style="height:80px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td style="text-align:center;">
+														<a href="https://decode-lab.com" title="logo" target="_blank">
+															<img width="80" src="https://i.ibb.co/Rh3XBsf/decode-lab.png" title="logo" alt="logo">
+														</a>
+														</td>
+													</tr>
+													<tr>
+														<td style="height:20px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td>
+															<table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+																style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+																<tr>
+																	<td style="height:40px;">&nbsp;</td>
+																</tr>
+																<tr>
+																	<td style="padding:0 35px;">
+																		<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">${user.name} you have
+																			requested to reset your password</h1>
+																		<span
+																			style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+																			We cannot simply send you your old password. A unique link to reset your
+																			password has been generated for you. To reset your password, click the
+																			following link and follow the instructions.
+																		</p>
+																		<a href=${resetPasswordLink}
+																			style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+																			Password</a>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px;">
+																			If you didn’t request a password reset, ignore this email. Your password will stay the same.
+																		</p>
+																		<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px; text-align: left;">
+																			Productively, </br> The Decode Lab Team
+																		</p>
+																	
+																	</td>
+																
+																</tr>
+																<tr>
+																	<td style="height:40px;">&nbsp;</td>
+																</tr>
+															</table>
+														</td>
+													<tr>
+														<td style="height:20px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td style="text-align:center;">
+															<p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.decode-lab.com</strong></p>
+														</td>
+													</tr>
+													<tr>
+														<td style="height:80px;">&nbsp;</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+								</body>
+								`
+							});
+
+							if (info.response) {
+								res.send({
+										status: true,
+										message: "email sent successfull",
+										data : resetPasswordLink,
+										statusCode: 200
+								})
+							}
+
+						}
+						sendmail();
+					}
 
 				}
 
@@ -764,18 +954,6 @@ const getUpcommingTaskByUserID = async (req, res) => {
 //send mail ok
 const sendMail = async (req, res) => {
 
-
-	// let transporter = nodemailer.createTransport({
-	// 	pool: true,
-	// 	host: "mail.wasa.decode-lab.com",
-	// 	port: 465,
-	// 	secure: true, // true for 465, false for other ports
-	// 	auth: {
-	// 		user: 'ashadul@wasa.decode-lab.com', // generated ethereal user
-	// 		pass: 'twNsCQdFKi3yLrs'
-	// 	},
-	// });
-
     try {
 
         let transporter = nodemailer.createTransport(smtpConfig);
@@ -791,8 +969,81 @@ const sendMail = async (req, res) => {
                     from: '<ashadul@wasa.decode-lab.com>', // sender address
                     to: 'ashadulmridhaprog@gmail.com',  // list of receivers
                     subject: "Reset your Project Management password", // Subject line
-                    text: "Reset your Project Management password", // plain text body
-                    html: `<a href=${link}>reset link ?</a>,` // html body
+   					html: `
+					<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    
+						<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+							style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+							<tr>
+								<td>
+									<table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+										align="center" cellpadding="0" cellspacing="0">
+										<tr>
+											<td style="height:80px;">&nbsp;</td>
+										</tr>
+										<tr>
+											<td style="text-align:center;">
+											<a href="https://rakeshmandal.com" title="logo" target="_blank">
+												<img width="80" src="https://i.ibb.co/Rh3XBsf/decode-lab.png" title="logo" alt="logo">
+											</a>
+											</td>
+										</tr>
+										<tr>
+											<td style="height:20px;">&nbsp;</td>
+										</tr>
+										<tr>
+											<td>
+												<table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+													style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+													<tr>
+														<td style="height:40px;">&nbsp;</td>
+													</tr>
+													<tr>
+														<td style="padding:0 35px;">
+															<h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">You have
+																requested to reset your password</h1>
+															<span
+																style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+															<p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+																We cannot simply send you your old password. A unique link to reset your
+																password has been generated for you. To reset your password, click the
+																following link and follow the instructions.
+															</p>
+															<a href="javascript:void(0);"
+																style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
+																Password</a>
+															<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px;">
+																If you didn’t request a password reset, ignore this email. Your password will stay the same.
+															</p>
+															<p style="color:#455056; font-size:15px;line-height:24px; margin-top: 17px; text-align: left;">
+																Productively, </br> The Decode Lab Team
+															</p>
+														
+														</td>
+													
+													</tr>
+													<tr>
+														<td style="height:40px;">&nbsp;</td>
+													</tr>
+												</table>
+											</td>
+										<tr>
+											<td style="height:20px;">&nbsp;</td>
+										</tr>
+										<tr>
+											<td style="text-align:center;">
+												<p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>www.decode-lab.com</strong></p>
+											</td>
+										</tr>
+										<tr>
+											<td style="height:80px;">&nbsp;</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+					</body>
+					`
                 });
 
                 if (info.response) {
